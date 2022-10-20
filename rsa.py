da656d9e60bc2071d9300b97048d3bb6981adec7
Daniela Blanco del Real <202108552@alu.comillas.edu>
2022-10-20 17:32:29 +0200

    rsa.py

---
+++ rsa.py
@@ -0,0 +1,23 @@
+import modular.py
+
+def generar_claves(min_primo, max_primo):
+    pass
+
+def aplicar_padding(m,digitos_padding):
+    pass
+
+def eliminar_padding(m,digitos_padding):
+    pass
+
+def cifrar_cadena_rsa(s,n,digitos_padding):
+    pass
+
+def descifrar_cadena_rsa(cList,n,d,digitos_padding):
+    pass
+
+def rompler_clave(n,e):
+    pass
+
+def ataque_texto_plano(cList,n,e):
+    pass
+
