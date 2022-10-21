cde31980f011f1d6dccb209c5b5d3df7b3cce14c
NatLey30 <112613481+NatLey30@users.noreply.github.com>
2022-10-21 20:01:30 +0200

    criptochat

--- criptochat.py
+++ criptochat.py
@@ -1,10 +1,82 @@
 import rsa
 import time
-if __name__ == '__main__':
+import os
 
+def menu():
+    print("MENÚ")
+    print("---------------------------------------------------------------------------------------")
+    print("1- Generar un nuevo par de claves pública-privada y registrarlo para el usuario del chat.")
+    print("2- Registrar un par de claves pública-privada para el usuario del chat.")
+    print("3- Registrar una clave pública para el destinatario de los mensajes.")
+    print("4- Cifrar un mensaje con la clave pública registrada para el destinatario.")
+    print("5- Descifrar un mensaje con la clave privada registrada para el usuario.")
+    print("6- Cambiar el número de cifras de padding utilizadas por RSA.")
+    print("7- Salir del programa.")
+    print("---------------------------------------------------------------------------------------")
+    try:
+        op = int(input("Eliga una de las 7 opciones: "))
+    except:
+        raise error
+    return op
+
+if __name__ == '__main__':
+    error = False
+    continuar = True
+
+    while continuar:
+        try:
+            op = menu()
+            if op == 1:
+                a = int(input("Introduzca un número: "))
+                b = int(input("Introduzca otro número: "))
+                claves = rsa.generar_claves(min(a,b), max(a,b))
+                existe = os.path.exists("misclaves.txt")
+                if existe:
+                    print("Se van a generar unas claves público-privadas nuevas y se van a borrar las antiguas para el usuario")
+                file = open("misclaves.txt", "w")
+                file.write(str(claves[0]) + "\n")
+                file.write(str(claves[1]) + "\n")
+                file.write(str(claves[2]))
+                file.close()
+            elif op == 2:
+                n = int(input("Introduzca el primer número de la clave pública (n): "))
+                e = int(input("Introduzca el segundo número de la clave pública (e): "))
+                d = int(input("Introduzca la clave privada (d): "))
+                if existe:
+                    print("Se van a registrar unas claves público-privadas nuevas y se van a borrar las antiguas para el usuario")
+                file = open("misclaves.txt", "w")
+                file.write(str(n) + "\n")
+                file.write(str(e) + "\n")
+                file.write(str(d))
+                file.close()
+            elif op == 3:
+                claves = input("Introduzca la clave publica, los dos ")
+                n = int(input("Introduzca la n: "))
+                e = int(input("Introduzca la n: "))
+                if existe:
+                    print("Se van a registrar unas claves privadas nuevas y se van a borrar las antiguas para el destinatario")
+                file = open("tusclaves.txt", "w")
+                file.write(str(n) + "\n")
+                file.write(str(e))
+                file.close()
+            elif op == 4:
+                ...
+            elif op == 5:
+                ...
+            elif op == 6:
+                ...
+            elif op == 7:
+                continuar = False
+            else:
+                print("No ha introducido una opción válida")
+        except:
+            print("AA No ha introducido una opción válida")
+
+
+    '''
     claves = rsa.generar_claves(16670,29860)
     print(claves)
-    '''
+    
     m = int(input("letra"))
     digitos_padding = 0
     c = rsa.cifrar_rsa(m,claves[0],claves[1],digitos_padding)
@@ -18,7 +90,7 @@ if __name__ == '__main__':
     print(cList)
     mensaje = rsa.descifrar_rsa(cList[0],claves[0],claves[2],0)
     print(rsa.ataque_texto_plano(cList,claves[0],claves[1]))
-    '''
+    
 
     i = time.time()
     n = 28282590191348679547
@@ -28,3 +100,4 @@ if __name__ == '__main__':
     print(rsa.ataque_texto_plano(cList,n,e))
     f  = time.time()
     print(f-i)
+    '''
